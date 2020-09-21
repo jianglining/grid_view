15b52fedc3ce38c6f7ec6caee732d4726e457f92
luodong <1873586010@qq.com>
2020-09-21 15:39:48 +0800

    ajax接口

--- vue.config.js
+++ vue.config.js
@@ -7,5 +7,16 @@ module.exports = {
   },
   transpileDependencies: [
     'quasar'
-  ]
+  ],
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://10.168.2.21:8090',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
+  }
 }
