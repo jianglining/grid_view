0ffe49582b420b520ca5e8725d2a6de4f5165dc8
chc <1143202578@qq.com>
2020-09-25 17:52:08 +0800

    完善开放服务各按钮功能

--- src/views/home/gridInfo.vue
+++ src/views/home/gridInfo.vue
@@ -462,7 +462,8 @@ export default {
         type: 'db_search'
       }
       fetchData(query)
-        .then((res) => {})
+        .then((res) => {
+        })
         .catch((error) => {
           console.log(error)
         })
